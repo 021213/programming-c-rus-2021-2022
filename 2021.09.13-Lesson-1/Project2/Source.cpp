f8a4d64d49dc1bf647b9f3f127878ed2b8fd2422
021213 <st021213@spbu.ru>
2021-09-13 12:16:27 +0300

    Minor code changes

--- 2021.09.13-Lesson-1/Project2/Source.cpp
+++ 2021.09.13-Lesson-1/Project2/Source.cpp
@@ -8,7 +8,8 @@ int main(int argc, char* argv[])
 	int a = 0;
 	int b = 0;
 	int c = 0;
-
+	
+	cout << "input a and b" << endl;
 	cin >> a >> b;
 	c = a + b;
 	cout << a << " + " << b << " = " << c << endl;
