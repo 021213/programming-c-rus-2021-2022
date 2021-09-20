7a2925e34eecfc59a9761efe682aeff906315084
021213 <st021213@spbu.ru>
2021-09-20 11:45:27 +0300

    Lesson 2 example code upload

--- 2021.09.20-Lesson-2(dist)/Project1/Source.cpp
+++ 2021.09.20-Lesson-2(dist)/Project1/Source.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	cout << "Hello world!" << endl;
+	int a = 0;
+	int b = 0;
+	cin >> a >> b;
+	cout << a << b << endl;
+	a ^= b ^= a ^= b;
+	cout << a << b << endl;
 	return EXIT_SUCCESS;
 }
