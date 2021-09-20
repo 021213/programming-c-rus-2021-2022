3821d736440495b6602287e5ee65e2eb1da80d1e
021213 <st021213@spbu.ru>
2021-09-20 10:54:36 +0300

    merging

---
+++ 2021.09.16-Lesson-1(2)/Project2/Source.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include<clocale>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a = 0;
+	int b = 0;
+	cout << "Введите числа для сложения : " << endl;
+	cin >> a >> b;
+	cout << "Сумма чисел равна : " << endl;
+	cout << a + b << endl;
+	return 0;
+}
