6536dadd0d02d219e000d88bfc86b22017292ffb
021213 <st021213@spbu.ru>
2022-04-25 12:27:04 +0300

    Lesson 8 code upload

---
+++ 2022.04.25-Lesson-8/Project1/Source.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include"LinkedList.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	LinkedList list;
+	cout << list << endl;
+	list.pushHead(0);
+	list.pushTail(1);
+	list.pushTail(2);
+	list.pushHead(-1);
+	list.pushHead(-2);
+	cout << list << endl;
+	cout << "poped : " << list.popHead() << endl;
+	cout << "poped : " << list.popHead() << endl;
+	cout << "poped : " << list.popTail() << endl;
+	cout << "poped : " << list.popTail() << endl;
+	cout << list << endl;
+	return EXIT_SUCCESS;
+}
